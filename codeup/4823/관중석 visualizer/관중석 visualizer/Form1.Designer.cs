f686a88566e641f533425faa29f4d3c00e4a8b86
inyeoptti <48206925+inyeoptti@users.noreply.github.com>
2020-07-25 00:18:32 +0900

    뻘짓

---
+++ codeup/4823/관중석 visualizer/관중석 visualizer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace 관중석_visualizer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.drawer = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 21);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "1";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(106, 0);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 21);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "5";
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// drawer
+			// 
+			this.drawer.Enabled = true;
+			this.drawer.Tick += new System.EventHandler(this.drawer_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 309);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(212, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 12);
+			this.label1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 335);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Timer drawer;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ codeup/4823/관중석 visualizer/관중석 visualizer/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 관중석_visualizer
+{
+	public partial class Form1 : Form
+	{
+		private int D1 = 0, D2 = 0;
+		private bool[,] chk;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			var count = 0;
+			
+			pictureBox1.Width = Width - 16;
+			pictureBox1.Height = Height - pictureBox1.Location.Y - 39;
+			if (int.TryParse(textBox1.Text, out D1) && int.TryParse(textBox2.Text, out D2))
+			{
+				chk = new bool[D2+1, D2+1];
+				if (D1 > 0 && D1 <= D2)
+				{
+					e.Graphics.DrawRectangle(Pens.Black, 0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+					e.Graphics.FillEllipse(Brushes.OrangeRed, pictureBox1.Width / 2 - 5, pictureBox1.Height / 2 - 5, 10, 10);
+					for(int i = D1; i<=D2; i++)
+					{
+						e.Graphics.DrawEllipse(Pens.Black, pictureBox1.Width / 2 - 10 * (i + 2), pictureBox1.Height / 2 - 10 * (i + 2), 20 * (i + 2), 20 * (i + 2));
+						for(int j = 1; j <= i; j++)
+						{
+							int k;
+							for (k = 1; !(k % i == 0 && k % j == 0); k++) ;
+							float angle = 360 / Convert.ToSingle(i) * j - 90;
+							
+							PointF dot = new PointF(pictureBox1.Width / 2 + Convert.ToSingle(Math.Cos(Math.PI * angle / 180.0)) * 10 * (i + 2) - 3, pictureBox1.Height / 2 + Convert.ToSingle(Math.Sin(Math.PI * angle / 180.0) * 10 * (i + 2))-3);
+							if (chk[k / i, k / j])
+							{
+								e.Graphics.DrawEllipse(Pens.Black, new RectangleF(dot, new SizeF(6, 6)));
+							}
+							else
+							{
+								e.Graphics.FillEllipse(Brushes.Black, new RectangleF(dot, new SizeF(6, 6)));
+								chk[k / i, k / j] = true;
+								count++;
+							}
+
+						}
+					}
+					label1.Text = "관객석 수 : " + count;
+
+					if (Width - 16 < 20 * (D2 + 2))
+					{
+						Width = 20 * (D2 + 2) + 16;
+					}
+					if (Height - pictureBox1.Location.Y - 39 < 20 * (D2 + 2))
+					{
+						Height = 20 * (D2 + 2) + pictureBox1.Location.Y + 39;
+					}
+				}
+				else
+				{
+					label1.Text = "관객석 수 : " + 0;
+				}
+			}
+			
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			pictureBox1.Refresh();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			pictureBox1.Refresh();
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			pictureBox1.Refresh();
+		}
+
+		private void drawer_Tick(object sender, EventArgs e)
+		{
+			//pictureBox1.Refresh();
+			
+		}
+	}
+}
